52dabbfb67c816fe0e6bb860e18f264f2568ef0c
Brandon Lim <brandondlim@gmail.com>
2016-10-30 18:16:33 -0700

    Add if-else statement for handling correct and incorrect answer selections.

--- GeoQuiz/QuizViewController.swift
+++ GeoQuiz/QuizViewController.swift
@@ -37,5 +37,11 @@ class QuizViewController: UIViewController {
     @IBAction func flagButtonPressed(sender: UIButton) {
 
         // TODO: Add code to display a message to the user telling them whether or not they guessed correctly.
+        if sender.tag == correctButtonTag {
+            displayAlert("Correct", messageText: "Good Job!")
+        } else {
+            displayAlert("Incorrect", messageText: "Try again!")
+        }
+        
     }
 }
